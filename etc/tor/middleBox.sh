1284bf3154d1e1e0952188e1bd28ecfafa593110
Mohammad Shokri <shokri.md@gmail.com>
2015-06-24 13:21:22 +0430

    Transparent Proxy for a MiddleBox

---
+++ etc/tor/middleBox.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Kernel IP forwarding MUST be disabled
+# @/etc/resolv.conf: nameserver 127.0.0.1
+
+# Tor's TransPort
+_trans_port="9040"
+
+# Your internal interface
+_int_if="eth0"
+
+iptables -F
+iptables -t nat -F
+
+iptables -t nat -A PREROUTING -i $_int_if -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -i $_int_if -p tcp --syn -j REDIRECT --to-ports $_trans_port
