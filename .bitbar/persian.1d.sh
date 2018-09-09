a085d43dd0fb9b19a3045222801ff95329501235
Mohammad Shokri <shokri.md@gmail.com>
2018-09-09 11:34:26 +0430

    :sparkles: Add support for persian cal via bitbar

---
+++ .bitbar/persian.1d.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# <bitbar.title>Persian</bitbar.title>
+# <bitbar.version>v0.0.1</bitbar.version>
+# <bitbar.author>Mohammad Shokri</bitbar.author>
+# <bitbar.author.github>slashsbin</bitbar.author.github>
+# <bitbar.desc>Persian Goodies</bitbar.desc>
+# <bitbar.dependencies></bitbar.dependencies>
+# <bitbar.image></bitbar.image>
+
+/usr/local/bin/php -r 'echo (new \IntlDateFormatter("fa_IR@calendar=persian", \IntlDateFormatter::FULL, \IntlDateFormatter::NONE, "Asia/Tehran", \IntlDateFormatter::TRADITIONAL))->format(new DateTime());'
